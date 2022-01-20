c9107982b5b72ebac2dc6bd2629594ad26200bb6
KOTBOTS <89208197+KOTBOTS@users.noreply.github.com>
2022-01-20 19:13:01 +0530

    Update script.py

--- script.py
+++ script.py
@@ -5,36 +5,45 @@
 
 class Script(object):
 
-    START_MSG = """<b>Hello {},
+    START_MSG = """<b>Hᴇʟʟᴏ {},
+I Cᴀɴ Exᴛʀᴀᴄᴛ Aᴜᴅɪᴏ Aɴᴅ Sᴜʙᴛɪᴛʟᴇs Fʀᴏᴍ Vɪᴅᴇᴏ Fɪʟᴇs.
 
-I can extract audio and subtitles from video files.
+Sᴇɴᴅ Mᴇ Aɴʏ Vɪᴅᴇᴏ Fɪʟᴇ Aɴᴅ Exᴛʀᴀᴄᴛ Wʜᴀᴛ Yᴏᴜ Wᴀɴᴛ
 
-Send me any video file and extract what you want :).
+Tʏᴘᴇ /help Fᴏʀ Hᴏᴡ Tᴏ Usᴇ Mᴇʜ
 
-See <i>help</i> for more details.
-
-© @KOT_BOTS</b>"""
-
-
-    HELP_MSG = """<b>Hai, Follow these Steps..</b>
-
-🌀 <i>Send me any Valid video file.</i>
-
-🌀 <i>Click Download and Process button to download the file to my server</i>
-
-🌀 <i>Wait till I process the video!</i>
-
-🌀 <i>Now select the stream you need to extract!</i>
-
-<b>© @KOT_BOTS</b>"""
+𝗕𝗢𝗧 𝗖𝗥𝗘𝗔𝗧𝗘𝗗 𝗔𝗡𝗗 𝗠𝗔𝗧𝗜𝗡𝗔𝗘𝗗 𝗕𝗬 @KOT_FREE_DE_LA_HOYA_OFF</b>"""
 
 
-    ABOUT_MSG = """⭕️<b>My Name : Streams Extractor Bot</b>
+    HELP_MSG = """<b>Hɪ Hᴏᴡ Tᴏ Usᴇ Mᴇʜ 👇</b>
 
-⭕️<b>Creater :</b> @KOT_BOTS    
+🌀 <b>Sᴇɴᴅ Mᴇ Aɴʏ Vᴀʟɪᴅ Vɪᴅᴇᴏ Fɪʟᴇ.</b>
 
-⭕️<b>Language :</b> <code>Python3</code>
+🌀 <b>Cʟɪᴄᴋ Dᴏᴡɴʟᴏᴀᴅ Aɴᴅ Pʀᴏᴄᴇss Bᴜᴛᴛᴏɴ Tᴏ Dᴏᴡɴʟᴏᴀᴅ Tʜᴇ Fɪʟᴇ Tᴏ Mʏ Sᴇʀᴠᴇʀ</b>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+🌀 <b>Wᴀɪᴛ Tɪʟʟ I Pʀᴏᴄᴇss Tʜᴇ Vɪᴅᴇᴏ!</b>
 
+🌀 <b>Nᴏᴡ Sᴇʟᴇᴄᴛ Tʜᴇ Sᴛʀᴇᴀᴍ Yᴏᴜ Nᴇᴇᴅ Tᴏ Exᴛʀᴀᴄᴛ!</i>
+
+<b>© 𝗕𝗢𝗧 𝗖𝗥𝗘𝗔𝗧𝗘𝗗 𝗔𝗡𝗗 𝗠𝗔𝗧𝗜𝗡𝗔𝗘𝗗 𝗕𝗬 @KOT_FREE_DE_LA_HOYA_OFF</b>"""
+
+
+    ABOUT_MSG = """
+╭──────[@KOT_BOTS]───────〄
+│
+├ Nᴀᴍᴇ : <a href='https://t.me/KOT_AUDIO_EXTRACT_BOT'>Kᴏᴛ Aᴜᴅɪᴏ Exᴛʀᴀᴄᴛ Bᴏᴛ</a>
+│
+├ Sᴇʀᴠᴇʀ : <a href='https://heroku.com/'>Hᴇʀᴏᴋᴜ</a>
+│ 
+├ Lᴀɴɢᴜᴀɢᴇ : <a href='https://docs.pyrogram.org/'>Pʏᴛʜᴏɴ 3.9.6</a>
+│
+├ Vᴇʀꜱɪᴏɴ : <a href='https://t.me/KOT_AUDIO_EXTRACT_BOT'>1.0 Bᴇᴛᴀ</a>
+│
+├ Fʀᴀᴍᴇᴡᴏʀᴋ : <a href='https://docs.pyrogram.org/'>Pʏʀᴏɢʀᴀᴍ 1.2.9</a>
+│
+├ Dᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/KOT_FREE_DE_LA_HOYA_OFF'>✯°• Kᴏᴛ Fʀᴇᴇ Dᴇ Lᴀ Hᴏʏᴀ Oғғ °•✯ | ✪ Bᴏᴛs CʀᴇᴀᴛᴏR ✪</a>
+│
+├ Pᴏᴡᴇʀᴇᴅ Bʏ : <a href='https://t.me/KOT_LINKS_TEAM'>Kᴏᴛ Lɪɴᴋs Tᴇᴀᴍ</a>
+│
+├ Uᴘᴅᴀᴛᴇᴅ Oɴ : [ 18.11.2021 ] 06:39 PM
 """
